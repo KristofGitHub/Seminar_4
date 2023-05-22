a989c10672e726cfc5eff75c672236d87589d895
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-22 13:07:37 +0500

    1st commit: first task

--- workshop/Program.cs
+++ workshop/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Написать программу, которая принимает на вход число и выдает количество цифр в числе.
+int dec = 10, result = 0;
+Console.WriteLine("Введите любое целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number/dec > 0) 
+{
+    number = number/10;
+    result++;
+}
+result++;
+Console.WriteLine($"В введённом вами числе {result} цифр(а/ы)");
