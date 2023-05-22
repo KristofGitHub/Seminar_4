87d15b4f2290dfb157efe60105cfd8717d0102b0
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-23 00:14:24 +0500

    initial commit: first task

---
+++ homework/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int result = 1;
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Произведение чисел ");
+for(int i = 1; i <= num; i++)
+{
+    result = i * result;
+    if (i == num) Console.Write($"{i}");
+    else Console.Write($"{i}*");
+}
+Console.WriteLine($" равно {result}");
