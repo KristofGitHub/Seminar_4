df1b18dc2f2ffc18120614fb44aeb03151c33a43
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-22 20:58:51 +0500

    2nd commit: second task

--- workshop/Program.cs
+++ workshop/Program.cs
@@ -1,11 +1,35 @@
 ﻿// 1. Написать программу, которая принимает на вход число и выдает количество цифр в числе.
-int dec = 10, result = 0;
-Console.WriteLine("Введите любое целое число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-while (number/dec > 0) 
+// int dec = 10, result = 0;
+// Console.WriteLine("Введите любое целое число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// while (number/dec > 0) 
+// {
+//     number = number/10;
+//     result++;
+// }
+// result++;
+// Console.WriteLine($"В введённом вами числе {result} цифр(а/ы)");
+
+// 2. Написать программу, которая бы выводила на экран массив из N элементов, заполненных целочисленными значениями от 0 до дохуя.
+
+Console.WriteLine("Введите размер массива: ");
+int array_size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимально возможное значение в ячейке: ");
+int min_value = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимально возможное значение в ячейке: ");
+int max_value = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
-    number = number/10;
-    result++;
+    int[] newArray = new int[size];
+    for(int index = 0; index < size; index++){newArray[index] = new Random().Next(minValue, maxValue + 1);}
+    return newArray;
 }
-result++;
-Console.WriteLine($"В введённом вами числе {result} цифр(а/ы)");
+void PrintArray(int[] arrayExample)
+{
+    int size = arrayExample.Length;
+    for(int index = 0; index < size; index++){Console.Write(arrayExample[index] + " ");}
+    Console.WriteLine();
+}
+int[] userArray = CreateRandomArray(array_size, min_value, max_value);
+PrintArray(userArray);
