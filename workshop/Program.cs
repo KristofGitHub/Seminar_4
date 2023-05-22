e573539fd5ba536bd19851ce1cc16d3e83b4b35a
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-23 00:10:17 +0500

    final commit: third task

--- workshop/Program.cs
+++ workshop/Program.cs
@@ -9,27 +9,45 @@
 // }
 // result++;
 // Console.WriteLine($"В введённом вами числе {result} цифр(а/ы)");
+// *******************************************************************************************************************************
 
 // 2. Написать программу, которая бы выводила на экран массив из N элементов, заполненных целочисленными значениями от 0 до дохуя.
 
-Console.WriteLine("Введите размер массива: ");
-int array_size = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите минимально возможное значение в ячейке: ");
-int min_value = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите максимально возможное значение в ячейке: ");
-int max_value = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите размер массива: ");
+// int array_size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимально возможное значение в ячейке: ");
+// int min_value = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимально возможное значение в ячейке: ");
+// int max_value = Convert.ToInt32(Console.ReadLine());
 
-int[] CreateRandomArray(int size, int minValue, int maxValue)
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] newArray = new int[size];
+//     for(int index = 0; index < size; index++){newArray[index] = new Random().Next(minValue, maxValue + 1);}
+//     return newArray;
+// }
+
+// void PrintArray(int[] arrayExample)
+// {
+//     int size = arrayExample.Length;
+//     for(int index = 0; index < size; index++){Console.Write(arrayExample[index] + " ");}
+//     Console.WriteLine();
+// }
+
+// int[] userArray = CreateRandomArray(array_size, min_value, max_value);
+// PrintArray(userArray);
+// *******************************************************************************************************************************
+
+// 3. Напишите программу, которая принимает число N и выдает произведение чисел от 1 до N.
+
+int result = 1;
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Произведение чисел ");
+for(int i = 1; i <= num; i++)
 {
-    int[] newArray = new int[size];
-    for(int index = 0; index < size; index++){newArray[index] = new Random().Next(minValue, maxValue + 1);}
-    return newArray;
+    result = i * result;
+    if (i == num) Console.Write($"{i}");
+    else Console.Write($"{i}*");
 }
-void PrintArray(int[] arrayExample)
-{
-    int size = arrayExample.Length;
-    for(int index = 0; index < size; index++){Console.Write(arrayExample[index] + " ");}
-    Console.WriteLine();
-}
-int[] userArray = CreateRandomArray(array_size, min_value, max_value);
-PrintArray(userArray);
+Console.WriteLine($" равно {result}");
